ba7fc24d12aff41361db7c8c2c2eb79d0730cb48
LoganM3 <mccalleylogan@gmail.com>
2022-07-26 16:57:51 -0400

    post new car

--- src/cars.js
+++ src/cars.js
@@ -12,5 +12,27 @@ export function getAllCars(req,res){
         res.send(cars)
 
     })
-    .catch(err => res.status(500).send(err))
+    .catch(err => handleError(err,res))
+}
+
+export function createCar(req, res){
+    // get a new car from request body
+    const newCar = req.body
+    //connect to the database
+    const db = dbConnect()
+    // add that car to cars collection
+    db.collection('cars').add(newCar)
+    .then(doc => {
+        res.status(201).send({
+            success:true,
+            id: doc.id
+        })
+    })
+    .catch(err => handleError(err,res))
+    //send back the new doc id 
+}
+
+function handleError(err,res){
+    console.error(err)
+    res.status(500).send(err)
 }
